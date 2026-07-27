9182cb3a42d9537703e8bb5a506d3b1008ca5aa1
Abhishek Parmar <abhishekparmar@webmynesystems.com>
2026-07-27 18:33:33 +0530

    style: remove Sanctum v4 Core and System Online status badge from footer

--- apps/api/resources/views/admin/includes/footer.blade.php
+++ apps/api/resources/views/admin/includes/footer.blade.php
@@ -3,9 +3,4 @@
   <div>
     &copy; {{ date('Y') }} Laraforge<span class="text-brand-500 font-bold">X</span>. All rights reserved.
   </div>
-  <div class="flex gap-4 mt-2 sm:mt-0 items-center">
-    <span>Sanctum v4 Core</span>
-    <span class="text-slate-200">•</span>
-    <span class="text-emerald-600"><i class="fa-solid fa-circle-check text-[10px] mr-1"></i> System Online</span>
-  </div>
 </footer>
